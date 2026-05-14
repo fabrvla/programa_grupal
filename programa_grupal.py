0064e056fc2de76ce9a295f60d16f6cac5a1bea8
Lorena Sanchez <sanlo26@outlook.es>
2026-05-13 19:49:57 -0500

    Commit 2- Lorena: agregué la función lorenaSanchez_funcion

--- programa_grupal.py
+++ programa_grupal.py
@@ -3,3 +3,11 @@ def presentar_grupo():
     print("Somos el Grupo 4 - Curso de Git/GitHub")
 
 presentar_grupo()
+
+# --- INTEGRANTE 2 - Lorena Sánchez Lozano ------
+def lorenaSanchez_funcion():
+	nombre= "Lorena Sánchez Lozano"
+	print(f"Hola, soy {nombre}")
+
+
+lorenaSanchez_funcion()
