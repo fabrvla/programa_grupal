b29ed72b9a9436005426a91de28dbbf1e80e4580
Joseph Angaspilco <josephangaspilco478@gmail.com>
2026-05-13 19:17:54 -0500

    Joseph: Actualizacion con funcion promedio

--- programa_grupal.py
+++ programa_grupal.py
@@ -3,3 +3,11 @@ def presentar_grupo():
     print("Somos el Grupo X - Curso de Git/GitHub")
 
 presentar_grupo()
+
+
+
+# ========== INTEGRANTE 4 ==========
+def calcular_promedio(notas):
+    return sum(notas) / len(notas)
+
+print("Promedio:", calcular_promedio([18, 19, 20, 17, 16, 20]))
