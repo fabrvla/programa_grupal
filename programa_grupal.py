51f7f0b04522df7c544aff6d89c36e54292c5b4e
Cristopher Salinas Montoya <salinasmontoya976@gmail.com>
2026-05-14 00:53:26 -0500

    commit 6 - cristopher: despedida

--- programa_grupal.py
+++ programa_grupal.py
@@ -1,5 +1,45 @@
-# ========== INTEGRANTE 1 ==========
+# ========== INTEGRANTE 1 - Fabiano ==========
 def presentar_grupo():
-    print("Somos el Grupo X - Curso de Git/GitHub")
+    print("Somos el Grupo 4 - Curso de Git/GitHub")
 
 presentar_grupo()
+
+# --- INTEGRANTE 2 - Lorena Sánchez Lozano ------
+def lorenaSanchez_funcion():
+	nombre= "Lorena Sánchez Lozano"
+	print(f"Hola, soy {nombre}")
+
+
+lorenaSanchez_funcion()
+
+# ===== INTEGRANTE 3 : Calderon Rondo  ==========
+
+def milenyCalderon_funcion():
+    nota = 20
+    print(f"Mi nota esperada es: {nota}")
+
+milenyCalderon_funcion()
+
+
+# ==========================================
+# INTEGRANTE 4: JOSEPH ANGASPIILCO JIMENEZ
+# ==========================================
+def calcular_promedio(notas):
+    return sum(notas) / len(notas)
+
+print("Promedio del grupo:", calcular_promedio([18, 19, 20, 17, 16, 20]))
+
+#------- INTEGRANTE 5: Edward Nuñez Vasquez ------
+
+def edwardNunez_function()
+    from datetime import date
+    print("Fecha: ", date.today())
+
+edwardNunez_function()
+
+
+# ========== INTEGRANTE 6: Cristopher Salinas Montoya ==========
+def despedida():
+    print("Fin del programa - Todos colaboramos!")
+
+despedida()
