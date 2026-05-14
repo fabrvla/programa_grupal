b4f95af005eaee7ebcf09e3c29ba47a052ecdcfa
Joseph Angaspilco <josephangaspilco478@gmail.com>
2026-05-13 20:03:25 -0500

    Joseph: Se agrego identificacion de Integrante 4 al codigo

--- programa_grupal.py
+++ programa_grupal.py
@@ -6,8 +6,11 @@ presentar_grupo()
 
 
 
-# ========== INTEGRANTE 4 ==========
+
+# ==========================================
+# INTEGRANTE 4: JOSEPH ANGASPIILCO JIMENEZ
+# ==========================================
 def calcular_promedio(notas):
     return sum(notas) / len(notas)
 
-print("Promedio:", calcular_promedio([18, 19, 20, 17, 16, 20]))
+print("Promedio del grupo:", calcular_promedio([18, 19, 20, 17, 16, 20]))
